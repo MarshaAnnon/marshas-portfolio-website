263b5a3d98db8c3c11777ca7216673e545f25c9b
Marsha Annon <marshaannon@Marshas-MacBook-Pro.local>
2020-01-10 13:42:09 -0500

    adding tablet and desktop styles

--- js/main.js
+++ js/main.js
@@ -48,6 +48,3 @@ const toggleSideMenu = n => {
 const closeSideMenuSelectingItem = (() => sideMenuItems.forEach((item) => item.addEventListener("click", closeSideMenu)))()
 sideMenuCloseDiv.addEventListener("click", closeSideMenu)
 
-var offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
-  offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-  scrollDuration = 700;
